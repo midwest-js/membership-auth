1dd8cdaeca2ab1d2880435cf0c71ed02c3a02976
Linus Miller <lohfu@lohfu.io>
2017-05-05 15:29:54 +0200

    Fix

--- index.js
+++ index.js
@@ -19,65 +19,62 @@ const responses = {
   },
 };
 
-module.exports = _.memoize((config) => {
-  return (req, res, next) => {
-    const { email, password, remember } = req.body;
+module.exports = _.memoize((config) => (req, res, next) => {
+  const { email, password, remember } = req.body;
 
-    config.getAuthenticationDetails(email.toLowerCase()).then((user) => {
-      let error;
+  config.getAuthenticationDetails(email.toLowerCase()).then((user) => {
+    let error;
 
-      if (user) {
-        if (!user.password) {
-          error = config.errors.login.notLocal;
-        } else if (!user.dateEmailVerified) {
-          error = config.errors.login.emailNotVerified;
-        } else if (user.dateBlocked) {
-          error = config.errors.login.blocked;
-        } else if (user.dateBanned) {
-          error = config.errors.login.banned;
-        } else {
-          return config.checkPassword(password, user.password).then(() => {
-            if (remember) {
-              if (config.remember.expires) {
-                req.session.cookie.expires = config.remember.expires;
-              } else {
-                req.session.cookie.maxAge = config.remember && config.remember.maxAge;
-              }
-            }
-
-            let promise = config.login(user);
-
-            if (config.hooks && config.hooks.login) {
-              promise = promise.then(config.hooks.login);
-            }
-
-            return promise.then(() => {
-              delete user.password;
-
-              res.status(200);
-
-              responses[req.accepts(['json', 'html'])](req, res, user);
-            });
-          });
-        }
+    if (user) {
+      if (!user.password) {
+        error = config.errors.login.notLocal;
+      } else if (!user.emailVerifiedAt) {
+        error = config.errors.login.emailNotVerified;
+      } else if (user.blockedAt) {
+        error = config.errors.login.blocked;
+      } else if (user.bannedAt) {
+        error = config.errors.login.banned;
       } else {
-        error = config.errors.login.noUserFound;
+        return config.checkPassword(password, user.password).then(() => {
+          if (remember) {
+            if (config.remember.expires) {
+              req.session.cookie.expires = config.remember.expires;
+            } else {
+              req.session.cookie.maxAge = config.remember && config.remember.maxAge;
+            }
+          }
+
+          let promise = config.login(req, user);
+
+          if (config.hooks && config.hooks.login) {
+            promise = promise.then(config.hooks.login);
+          }
+
+          return promise.then(() => {
+            delete user.password;
+
+            res.status(200);
+
+            responses[req.accepts(['json', 'html'])](req, res, user);
+          });
+        });
       }
+    } else {
+      error = config.errors.login.noUserFound;
+    }
 
-      throw createError(...error);
-    }).catch((err) => {
-      if (req.body.password) {
-        req.body.password = 'DELETED';
-      }
+    throw createError(...error);
+  }).catch((err) => {
+    if (req.body.password) {
+      req.body.password = 'DELETED';
+    }
 
-      // TODO this should probably not even be sent to the server
-      // or maybe it should, especially if non-js login is required
-      if (req.body.confirmPassword) {
-        req.body.confirmPassword = 'DELETED';
-      }
+    // TODO this should probably not even be sent to the server
+    // or maybe it should, especially if non-js login is required
+    if (req.body.confirmPassword) {
+      req.body.confirmPassword = 'DELETED';
+    }
 
-      next(err);
-    });
-  };
-
-})
+    next(err);
+  });
+});
